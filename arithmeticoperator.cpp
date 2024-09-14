5469cba1a9794c8972a04d00e3b9941665b5c715
skylover646 <akpatel64000@gmail.com>
2024-09-14 20:27:39 +0530

    logical file add

---
+++ arithmeticoperator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=10, b=5;
+// or write like  cout this int sum=a+b;
+//cout <<sum<<endl;
+cout<<"sum=" <<(a+b)<<endl;
+cout<<"difference="<<(a-b)<<endl;
+cout<<"product="<<(a*b)<<endl;
+cout << "division="<<(a/b)<<endl;
+cout<<"modulo="<<(a%b)<<endl;
+    return 0;
+}
---
+++ realtionaloperator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<(3<5)<<endl;//ans 1
+cout<<(3>5)<<endl;
+cout<<(3<=3)<<endl;//ans 1 becuse its true value
+cout<<(3>=3)<<endl;//yes 3>=3 ans 1
+cout <<(3!=5)<<endl;
+
+    return 0;
+}
