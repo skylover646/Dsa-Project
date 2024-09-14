424f7fabd6bb7d955fcacae2967e11d7c181abd7
skylover646 <akpatel64000@gmail.com>
2024-09-14 16:58:58 +0530

    three files

--- bool.cpp
+++ bool.cpp
@@ -5,8 +5,8 @@ int main()
 int value=45;
 char grade='A';
 float pi = 3.14f;
-bool isSafe=false;
-//bool isSafe =true;
+bool isSafe=false;//when it is false value is 0
+//bool isSafe =true; //it is ture value is 1
 cout<<isSafe<<endl;
 
     return 0;
