9cf3d72c1f36c914654dcb50a7033295c6fef5aa
skylover646 <akpatel64000@gmail.com>
2024-09-14 20:31:46 +0530

    new one add

---
+++ logicaloperator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout <<(3<5)<<endl;//ans is true if i am add !(exclamation mark -not logical value)
+cout<<!(3<5)<<endl;
+cout<<((3<5)||(3>5))<<endl;// it is or logical.for when we want value always true or false when condition is one value true or false we have 
+cout <<((3<5)&&(3>5))<<endl;//if one value is not ture ans false and first and second value is true than answer is true in
+//and logical
+    return 0;
+}
