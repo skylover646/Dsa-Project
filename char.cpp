909562dcc67bc8beceb4c78d898f2761c63d41bf
skylover646 <akpatel64000@gmail.com>
2024-09-14 11:48:29 +0530

    new file add

---
+++ char.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 45;
+    char grade ="A";
+    cout << grade << endl;
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=40;
+    //cout <<age << endl;
+    //how to know what size of this stored data ?
+    cout << sizeof(age) <<endl;
+    //it is int variable show its value is 4byte so answer is 4.
+    return 0;
+}
