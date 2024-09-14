341bcd32fe32229e163c752cbb978eb0724f5174
skylover646 <akpatel64000@gmail.com>
2024-09-14 17:33:25 +0530

    conversion file add

---
+++ conversion.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char grade ='A'; // when we print value it is show in ascii value by what we print like a capital or small
+// char grade ='a';  
+
+int value = grade;
+cout <<value <<endl;
+    return 0;
+}
