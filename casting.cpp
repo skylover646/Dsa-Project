2ae449a36d45479afd5458b859d8f832f4c2d574
skylover646 <akpatel64000@gmail.com>
2024-09-14 17:43:05 +0530

    casting file

---
+++ casting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+ double price =100.99;
+
+ int newPrice =(int)price;//don't shown in decimal value when it's printout.
+ cout<<newPrice<<endl;
+    return 0;
+}
