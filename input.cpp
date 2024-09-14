524e5812d13f2c8db672bef8ecc838464953b9b6
skylover646 <akpatel64000@gmail.com>
2024-09-14 18:14:56 +0530

    add files

---
+++ input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int age;
+cout << "Enter your age : ";
+cin >>age;
+cout <<"your age is :"<<age<<endl;
+//cout <<age<<endl;//showing when we print it is call garbage value.
+    return 0;
+}
