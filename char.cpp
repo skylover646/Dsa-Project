2aedd1561cedb0b43c7f2b58430ce32c00265c5f
skylover646 <akpatel64000@gmail.com>
2024-09-14 11:59:40 +0530

    new file add

--- char.cpp
+++ char.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int age = 45;
-    char grade ="A";
+    char grade ='A';
     cout << grade << endl;
     return 0;
 }
---
+++ float.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int value = 45;
+char grade='A';
+// char value always write in '' single qoute.
+float pi = 3.14f;
+cout << pi<<endl;
+    return 0;
+}
