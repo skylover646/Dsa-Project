3a6b8da4a11273c541e17cd1ebc3c61a38cbf068
skylover646 <akpatel64000@gmail.com>
2024-09-15 22:48:12 +0530

    ifelse

---
+++ ifelse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n=80;// n=-80;
+if (n >=0){
+    cout <<"n is postive \n"; 
+}
+else {
+    cout <<"n is negative \n";
+}
+
+    return 0;
+}
